c496fc46452848f548d6b522244a75aabb046148
TheTrepanier_Dev <TheTrepanier@icloud.com>
2021-01-31 07:48:26 -0700

    init commit

---
+++ WeSplit-hacking_with_swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit-hacking_with_swift
+//
+//  Created by Michael Trepanier on 1/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit-hacking_with_swift/WeSplit_hacking_with_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_hacking_with_swiftApp.swift
+//  WeSplit-hacking_with_swift
+//
+//  Created by Michael Trepanier on 1/31/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_hacking_with_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
