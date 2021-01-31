8c4ec367be1e4f4847d0a4704bce7828c3fd8a12
TheTrepanier_Dev <TheTrepanier@icloud.com>
2021-01-31 09:15:39 -0700

    completion of basic project

---
+++ README.md
@@ -0,0 +1,10 @@
+## WeSplit
+[Paul Hudson](https://www.hackingwithswift.com/about) * [@twostraws](https://twitter.com/twostraws)
+
+### Intro
+The purpose of this project is to familiarize myself with the basics of UI design, let users enter values, select from options, and the basics of SwiftUI. 
+
+### Challenges
+- [ ] Add a header to the third section, saying "Amount per person"
+- [ ] Add another section showing the total amount for the check -i.e., the original amount plus tip value, without dividing by the number of people
+- [ ] Change the "Number of people" picker to be a text field, making sure to use the correct keyboard type
--- WeSplit-hacking_with_swift/ContentView.swift
+++ WeSplit-hacking_with_swift/ContentView.swift
@@ -4,18 +4,70 @@
 //
 //  Created by Michael Trepanier on 1/31/21.
 //
+//  Group {} is to create manageable groups of children within a parent object like a form
+//  Section {} is to create a visually segmented group of children within a form (smiliar to the settings app in iOS)
+//
+//  @State is specifically designed for simple properties that are stored in one view. As a result, Apple recommends we add private access control to those properties
+//
+//  The $ is used to flag a value as being double binded, meaning that the TextField will show the value of the property but also will update the value of the property for any changes made to the text field
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalePerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                .textCase(.none)
+                Section {
+                    Text("$\(totalePerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
